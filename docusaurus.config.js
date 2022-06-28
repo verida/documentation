b1454bb98e41f115cce620f3932fb6afee907ed4
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2022-06-28 14:30:08 +0100

    feat: updated footer styling - Added google fonts assets #104

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,144 +1,158 @@
 // @ts-nocheck
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'Verida Developers',
-    tagline: 'Welcome to your Web3 toolkit!',
-    url: 'https://developers.verida.io',
-    baseUrl: '/',
-    onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/new_favicon.ico',
-    organizationName: 'verida', // Usually your GitHub org/user name.
-    projectName: 'verida', // Usually your repo name.
-    trailingSlash: false,
-    themes: ['@docusaurus/theme-live-codeblock'],
-    presets: [
-        [
-            '@docusaurus/preset-classic',
-            /** @type {import('@docusaurus/preset-classic').Options} */
-            ({
-                docs: {
-                    sidebarPath: require.resolve('./sidebars.js'),
-                    remarkPlugins: [
-                        (require('@docusaurus/remark-plugin-npm2yarn'), { sync: true }),
-                        (require('mdx-mermaid')),
-                    ],
-                },
-                sitemap: {
-                    changefreq: 'weekly',
-                    priority: 0.5,
-                },
-                pages: {
-                    remarkPlugins: [require('@docusaurus/remark-plugin-npm2yarn')],
-                },
-                blog: false,
-                theme: {
-                    customCss: require.resolve('./src/css/custom.css'),
-                },
-            }),
-        ],
-    ],
-    scripts: [
-        {
-            src: 'https://plausible.io/js/plausible.js',
-        },
-    ],
-    themeConfig:
-        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-        ({
-            colorMode: {
-                defaultMode: 'light',
-                disableSwitch: true,
-                respectPrefersColorScheme: false,
-                switchConfig: {
-
-                },
-            },
-            metadata: [
-                        { name: 'keywords', content: 'verida, personal data'},
-                        {name: 'robots', content: 'max-image-preview:large'},
-                    ],
-            navbar: {
-                //title: 'Verida',
-                logo: {
-                    alt: 'Verida Logo',
-                    src: 'img/new_logo.svg',
-                },
-                items: [
-                    {
-                        type: 'doc',
-                        docId: 'intro',
-                        position: 'left',
-                        label: 'Documentation',
-                    },
-                    {
-                        href: 'https://verida.io',
-                        label: 'Verida Homepage',
-                        position: 'left',
-                    },
-                    // {to: '/blog', label: 'Blog', position: 'left'},
-                    {
-                        href: 'https://github.com/verida',
-                        label: 'GitHub',
-                        position: 'right',
-                    },
-                ],
-            },
-            image: 'img/verida_social_preview2.png',
-            footer: {
-                style: 'dark',
-                links: [
-                    {
-                        title: 'Docs',
-                        items: [
-                            {
-                                label: 'Verida Developers',
-                                to: '/docs/intro',
-                            }
-                        ],
-                    },
-                    {
-                        title: 'Community',
-                        items: [
-                            {
-                                label: 'Discord',
-                                href: 'https://discord.com/invite/gBzTSzMCNA',
-                            },
-                        ],
-                    },
-                    {
-                        title: 'More',
-                        items: [
-                            {
-                                label: 'GitHub',
-                                href: 'https://github.com/verida',
-                            },
-                        ],
-                    },
-                ],
-                copyright: `Copyright © ${new Date().getFullYear()} Verida. Built with Docusaurus.`,
-            },
-            prism: {
-                theme: lightCodeTheme,
-                darkTheme: darkCodeTheme,
-            },
-        }),
-    plugins: [
-        [
-            require.resolve("@cmfcmf/docusaurus-search-local"),
-            {
-                // Options here
-                indexBlog: false,
-                indexPages: false,
-            },
-        ],
-        './webpack_plugin',
-    ],
+	title: "Verida Developers",
+	tagline: "Welcome to your Web3 toolkit!",
+	url: "https://developers.verida.io",
+	baseUrl: "/",
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
+	favicon: "img/new_favicon.ico",
+	organizationName: "verida", // Usually your GitHub org/user name.
+	projectName: "verida", // Usually your repo name.
+	trailingSlash: false,
+	themes: ["@docusaurus/theme-live-codeblock"],
+	presets: [
+		[
+			"@docusaurus/preset-classic",
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					sidebarPath: require.resolve("./sidebars.js"),
+					remarkPlugins: [
+						(require("@docusaurus/remark-plugin-npm2yarn"), { sync: true }),
+						require("mdx-mermaid"),
+					],
+				},
+				sitemap: {
+					changefreq: "weekly",
+					priority: 0.5,
+				},
+				pages: {
+					remarkPlugins: [require("@docusaurus/remark-plugin-npm2yarn")],
+				},
+				blog: false,
+				theme: {
+					customCss: require.resolve("./src/css/custom.css"),
+				},
+			}),
+		],
+	],
+	scripts: [
+		{
+			src: "https://plausible.io/js/plausible.js",
+		},
+	],
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			colorMode: {
+				defaultMode: "light",
+				disableSwitch: true,
+				respectPrefersColorScheme: false,
+				switchConfig: {},
+			},
+			metadata: [
+				{ name: "keywords", content: "verida, personal data" },
+				{ name: "robots", content: "max-image-preview:large" },
+			],
+			navbar: {
+				//title: 'Verida',
+				logo: {
+					alt: "Verida Logo",
+					src: "img/new_logo.svg",
+				},
+				items: [
+					{
+						type: "doc",
+						docId: "intro",
+						position: "left",
+						label: "Documentation",
+					},
+					{
+						href: "https://verida.io",
+						label: "Verida Homepage",
+						position: "left",
+					},
+					// {to: '/blog', label: 'Blog', position: 'left'},
+					{
+						href: "https://github.com/verida",
+						label: "GitHub",
+						position: "right",
+					},
+				],
+			},
+			image: "img/verida_social_preview2.png",
+			footer: {
+				style: "light",
+				links: [
+					{
+						title: "Product",
+						items: [
+							{ label: "Solutions", href: "https://www.verida.io/solutions" },
+							{ label: "Get App", href: "https://www.verida.io/user-vault" },
+						],
+					},
+					{
+						title: "Company",
+						items: [
+							{ label: "About", href: "https://www.verida.io/about-us" },
+							{ label: "Careers", href: "https://www.verida.io/careers" },
+							{ label: "Blog", href: "https://news.verida.io" },
+						],
+					},
+					{
+						title: "Community",
+						items: [
+							{ label: "Telegram", href: "https://t.me/verida_community" },
+							{ label: "Twitter", href: "https://twitter.com/verida_io" },
+							{ label: "Discord", href: "https://discord.com/invite/qb6vS43" },
+							{ label: "Medium", href: "https://medium.com/verida" },
+							{
+								label: "LinkedIn",
+								href: "https://www.linkedin.com/company/verida-technology/",
+							},
+						],
+					},
+					{
+						title: "Developer",
+						items: [
+							{
+								label: "Develop with verida",
+								href: "https://www.verida.io/verida-developers",
+							},
+							{
+								label: "Whitepaper",
+								href: "https://www.verida.io/whitepaper?utm=devfooter",
+							},
+							{ label: "Documentation", href: "https://developers.verida.io/" },
+							{ label: "Github", href: "https://github.com/verida/" },
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} Verida. Built with Docusaurus.`,
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+			},
+		}),
+	plugins: [
+		[
+			require.resolve("@cmfcmf/docusaurus-search-local"),
+			{
+				// Options here
+				indexBlog: false,
+				indexPages: false,
+			},
+		],
+		"./webpack_plugin",
+	],
 };
 
 module.exports = config;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,126 +1,131 @@
-import React, { useState } from 'react';
-import Link from '@docusaurus/Link';
-import AppLogo from '@site/static/img/verida_logo_full.svg';
-import styles from './footer.module.css';
+import React, { useState } from "react";
+import Link from "@docusaurus/Link";
+import AppLogo from "@site/static/img/verida_logo_full.svg";
+import styles from "./footer.module.css";
 
 export const products = [
-    { title: "Solutions", link: "https://www.verida.io/solutions" },
-    { title: "Get App", link: "https://www.verida.io/user-vault" },
+	{ title: "Solutions", link: "https://www.verida.io/solutions" },
+	{ title: "Get App", link: "https://www.verida.io/user-vault" },
 ];
 
 export const company = [
-    { title: "About", link: "https://www.verida.io/about-us" },
-    { title: "Careers", link: "https://www.verida.io/careers" },
-    { title: "Blog", link: "https://news.verida.io" },
+	{ title: "About", link: "https://www.verida.io/about-us" },
+	{ title: "Careers", link: "https://www.verida.io/careers" },
+	{ title: "Blog", link: "https://news.verida.io" },
 ];
 
 export const developers = [
-    {
-        title: "Develop with verida",
-        link: "https://www.verida.io/verida-developers",
-    },
-    {
-        title: "Whitepaper",
-        link: "https://www.verida.io/whitepaper?utm=devfooter",
-    },
-    { title: "Documentation", link: "https://developers.verida.io/" },
-    { title: "Github", link: "https://github.com/verida/" },
+	{
+		title: "Develop with verida",
+		link: "https://www.verida.io/verida-developers",
+	},
+	{
+		title: "Whitepaper",
+		link: "https://www.verida.io/whitepaper?utm=devfooter",
+	},
+	{ title: "Documentation", link: "https://developers.verida.io/" },
+	{ title: "Github", link: "https://github.com/verida/" },
 ];
 
 export const community = [
-    { title: "Telegram", link: "https://t.me/verida_community" },
-    { title: "Twitter", link: "https://twitter.com/verida_io" },
-    { title: "Discord", link: "https://discord.com/invite/qb6vS43" },
-    { title: "Medium", link: "https://medium.com/verida" },
-    {
-        title: "LinkedIn",
-        link: "https://www.linkedin.com/company/verida-technology/",
-    },
+	{ title: "Telegram", link: "https://t.me/verida_community" },
+	{ title: "Twitter", link: "https://twitter.com/verida_io" },
+	{ title: "Discord", link: "https://discord.com/invite/qb6vS43" },
+	{ title: "Medium", link: "https://medium.com/verida" },
+	{
+		title: "LinkedIn",
+		link: "https://www.linkedin.com/company/verida-technology/",
+	},
 ];
 
-
 const footerNav = {
-    products: {
-        title: "Products",
-        list: products,
-    },
-    company: {
-        title: "Company",
-        list: company,
-    },
-    developers: {
-        title: "For Developers",
-        list: developers,
-    },
-    community: {
-        title: "Community",
-        list: community,
-    },
-}
+	products: {
+		title: "Products",
+		list: products,
+	},
+	company: {
+		title: "Company",
+		list: company,
+	},
+	developers: {
+		title: "For Developers",
+		list: developers,
+	},
+	community: {
+		title: "Community",
+		list: community,
+	},
+};
 
 const Footer = () => {
-    const [state] = useState(footerNav)
-    return (
-        <footer className={styles.footer} >
-            <div className={styles.footerSection}>
-                <div className="footer-links-img">
-                    <a href="https://verida.io" >
-                        <AppLogo title="verida" />
-                    </a>
-                </div>
+	const [state] = useState(footerNav);
+	return (
+		<footer className={styles.footer}>
+			<div className={styles.footerSection}>
+				<div className='footer-links-img'>
+					<a href='https://verida.io'>
+						<AppLogo title='verida' />
+					</a>
+				</div>
 
-                <div className="footer-links-nav">
-                    <h5>{state.products.title}</h5>
-                    <ul >
-                        {state.products.list.map(item => (
-                            <li key={item.title}>
-                                <Link href={item.link} >{item.title}</Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div >
-                <div className="footer-links-nav">
-                    <h5>{state.company.title}</h5>
-                    <ul >
-                        {state.products.list.map(item => (
-                            <li key={item.title}>
-                                <Link href={item.link} >{item.title}</Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div >
-                <div className="footer-links-nav">
-                    <h5>{state.developers.title}</h5>
-                    <ul >
-                        {state.developers.list.map(item => (
-                            <li key={item.title}>
-                                <Link href={item.link} >{item.title}</Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div >
-                <div className="footer-links-nav">
-                    <h5>{state.company.title}</h5>
-                    <ul >
-                        {state.company.list.map(item => (
-                            <li key={item.title}>
-                                <Link href={item.link} >{item.title}</Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div >
-            </div>
+				<div className='footer-links-nav'>
+					<h5>{state.products.title}</h5>
+					<ul>
+						{state.products.list.map((item) => (
+							<li key={item.title}>
+								<Link href={item.link}>{item.title}</Link>
+							</li>
+						))}
+					</ul>
+				</div>
+				<div className='footer-links-nav'>
+					<h5>{state.company.title}</h5>
+					<ul>
+						{state.products.list.map((item) => (
+							<li key={item.title}>
+								<Link href={item.link}>{item.title}</Link>
+							</li>
+						))}
+					</ul>
+				</div>
+				<div className='footer-links-nav'>
+					<h5>{state.developers.title}</h5>
+					<ul>
+						{state.developers.list.map((item) => (
+							<li key={item.title}>
+								<Link href={item.link}>{item.title}</Link>
+							</li>
+						))}
+					</ul>
+				</div>
+				<div className='footer-links-nav'>
+					<h5>{state.community.title}</h5>
+					<ul>
+						{state.community.list.map((item) => (
+							<li key={item.title}>
+								<Link href={item.link}>{item.title}</Link>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
 
+			<div className={styles.footerBottom}>
+				<hr className={styles.footerLine} />
+				<div className={styles.footerBottomContent}>
+					<span className='text-white' href='##'>
+						@&nbsp;Verida&nbsp;{new Date().getFullYear()}
+					</span>
+					<a
+						className='text-white'
+						href='https://www.verida.io/terms-and-conditions'
+					>
+						Terms&nbsp;&&nbsp;Conditions
+					</a>
+				</div>
+			</div>
+		</footer>
+	);
+};
 
-            <div className={styles.footerBottom}>
-                <hr className={styles.footerLine} />
-                <div className={styles.footerBottomContent}>
-                    <a className="text-white" href="##">@Verida{new Date().getFullYear()}</a>
-                    <a className="text-white" href="##">Terms&Conditions</a>
-                </div>
-            </div>
-        </footer>
-    )
-}
-
-export default Footer
+export default Footer;
