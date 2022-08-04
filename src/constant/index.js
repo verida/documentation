3b5e3c085e577c87a42cdc0446f14f0fedbc43cb
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2022-08-04 05:22:15 +0100

    fix:added andriod links for playstore #113

---
+++ src/constant/index.js
@@ -0,0 +1,43 @@
+const APPLE_STORE_LINK = `https://apps.apple.com/us/app/verida-vault/id1546599632`;
+const GOOGLE_PLAY_STORE_LINK = `https://apps.apple.com/us/app/verida-vault/id1546599632`;
+
+const GITHUB_LINKS = [
+	{
+		title: "Verida-Js",
+		link: "https://github.com/verida/verida-js",
+	},
+	{
+		title: "Markdown Notes Demo",
+		link: "https://github.com/verida/markdown-notes-demo",
+	},
+	{
+		title: "Documentation",
+		link: "https://github.com/verida/documentation",
+	},
+	{
+		title: "Storage-Node",
+		link: "https://github.com/verida/storage-node",
+	},
+	{
+		title: "Account Explorer",
+		link: "https://github.com/verida/account-explorer",
+	},
+	{
+		title: "Generic-demo",
+		link: "https://github.com/verida/generic-demo",
+	},
+];
+
+const HOME_HEAD_DESCRIPTION = `
+	Explore the Verida Developer's Portal to get 
+	started with our developer documentation,
+	explore our demos, 
+	try out our interactive tutorials and connect with the Web3 developer community.
+`;
+
+export {
+	APPLE_STORE_LINK,
+	HOME_HEAD_DESCRIPTION,
+	GITHUB_LINKS,
+	GOOGLE_PLAY_STORE_LINK,
+};
